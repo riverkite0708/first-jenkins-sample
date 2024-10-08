2e994faf9fd51902e06fce4d3d1f0ae04d722336
riverkite0708 <0908xmrqkr@gmail.com>
2024-10-08 15:53:18 +0900

    edit ver2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Hello, World!";
+        return "Hello, World!!! ver2 ";
     }
 }
